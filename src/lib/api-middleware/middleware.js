f439335e8e2323282c5535978ffa99c7423fe692
Mario Solano <marioqwe@vt.edu>
2019-02-13 23:17:48 -0500

    remove jwt reducer

--- src/lib/api-middleware/middleware.js
+++ src/lib/api-middleware/middleware.js
@@ -17,41 +17,46 @@ export const middleware = ({ dispatch, getState }) => {
             method,
             actions,
             data,
+            devMode = false,
         } = _props;
 
         const [requestAction, successAction, failureAction] = actions;
         next(requestAction);
 
-        return axios({
-            url,
-            method,
-            headers,
-            data,
-        })
-            .then(response => next({
-                ...successAction,
-                payload: {...response.data},
-            }), (error) => {
-                if (error.code === 'ECONNABORTED'
-                    || (error.response !== undefined && error.response.status === 408)) {
+        if (!devMode) {
+            return axios({
+                url,
+                method,
+                headers,
+                data,
+            })
+                .then(response => next({
+                    ...successAction,
+                    payload: {...response.data},
+                }), (error) => {
+                    if (error.code === 'ECONNABORTED'
+                        || (error.response !== undefined && error.response.status === 408)) {
+                        return next({
+                            ...failureAction,
+                            type: CONNECTION_TIMEOUT,
+                        });
+                    }
+
+                    if (error.response === undefined) {
+                        return next({
+                            ...failureAction,
+                            type: NO_INTERNET_CONNECTION,
+                        });
+                    }
+
                     return next({
                         ...failureAction,
-                        type: CONNECTION_TIMEOUT,
+                        payload: {...error.response.data},
                     });
-                }
-
-                if (error.response === undefined) {
-                    return next({
-                        ...failureAction,
-                        type: NO_INTERNET_CONNECTION,
-                    });
-                }
-
-                return next({
-                    ...failureAction,
-                    payload: {...error.response.data},
                 });
-            });
+        } else {
+
+        }
     };
 };
 
