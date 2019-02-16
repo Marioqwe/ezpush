be7a16346903fe43da2ed3da2e52ab866bed1faf
Mario Solano <marioqwe@vt.edu>
2019-02-16 13:23:10 -0500

    add semicolon

--- src/site/index.js
+++ src/site/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import CssBaseline from '@material-ui/core/CssBaseline';
 
