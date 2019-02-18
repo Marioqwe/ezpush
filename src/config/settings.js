b4ee83958db9ab604bdce8deba42a7b3bb9d63ba
Mario Solano <marioqwe@vt.edu>
2019-02-17 23:23:51 -0500

    isProduction -> DEBUG

--- src/config/settings.js
+++ src/config/settings.js
@@ -1,1 +1,3 @@
 export const BASE_URL = 'https://api.ezpush.io';
+
+export const DEBUG = !(process.env.NODE_ENV === 'production');
--- src/site/auth/redux/actions.js
+++ src/site/auth/redux/actions.js
@@ -1,15 +1,13 @@
-import { BASE_URL } from 'config/settings';
+import { BASE_URL, DEBUG } from 'config/settings';
 import { CALL_API } from 'lib/api-middleware/middleware';
 import * as types from './types';
-
-const isProduction = (process.env.NODE_ENV === 'production');
 
 export const login = (email, password) => ({
     [CALL_API]: {
         url: `${BASE_URL}/auth/token/obtain`,
         method: 'POST',
         data: { email, password },
-        devMode: !isProduction,
+        devMode: DEBUG,
         mockResponse: { 'foo': 'bar' },
         actions: [
             types.LOGIN_REQUEST,
